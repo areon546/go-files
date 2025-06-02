4988dc1d3aeaa285a143e91af6bd2ea34ffbb0b5
areon546 <57325743+areon546@users.noreply.github.com>
2025-06-02 13:43:12 +0100

    remove unnecessary print

--- files/readDirectory.go
+++ files/readDirectory.go
@@ -8,8 +8,6 @@ import (
 
 // This function
 func ReadDirectory(dirPath string) (entries []fs.DirEntry) {
-	debugPrint("Reading directory ", dirPath)
-
 	// read contents of a directory
 	entries, err := os.ReadDir(dirPath)
 	if err != nil {
