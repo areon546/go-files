c381fafc09f429b71f1c1c5a05c3d9c05f683fcf
areon546 <57325743+areon546@users.noreply.github.com>
2025-06-30 10:31:43 +0100

    add a FileExists method

--- files/readDirectory.go
+++ files/readDirectory.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"errors"
 	"io/fs"
 	"log"
 	"os"
@@ -15,6 +16,21 @@ func ReadDirectory(dirPath string) (entries []fs.DirEntry) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	return
+}
+
+// Checks if a specific file exists within the file system or not.
+// If it does exist, it returns true, and the os.FileInfo entry that can be gained by os.Stat.
+// If it does not exist, it returns false, nill.
+func FileExists(path string) (exists bool, info os.FileInfo) {
+	info, err := os.Stat(path)
+
+	if errors.Is(err, fs.ErrNotExist) {
+		exists = false
+	} else {
+		exists = true
+	}
+
 	return
 }
 
