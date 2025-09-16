b03b136b522a3a2b46b179512724503a8f11c13b
areon546 <57325743+areon546@users.noreply.github.com>
2025-09-16 11:28:02 +0100

    add sqlite tests

--- files/sqlite/sqlite_test.go
+++ files/sqlite/sqlite_test.go
@@ -4,16 +4,29 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/areon546/go-files/files"
 	"github.com/areon546/go-helpers/helpers"
 )
 
 func Test(t *testing.T) {
-	fn := "file:sqlite.db"
+	files.RemoveAllWithinDirectory("./test")
+
+	fn := "file:./test/sqlite.db"
 
 	n, _ := Open(fn)
 
 	fmt.Println(n)
-	n.Query("CREATE TABLE IF NOT EXISTS skins (id, name);")
+	res, err := n.Exec("CREATE TABLE IF NOT EXISTS test (name);")
+	helpers.AssertNoError(t, err)
 
-	helpers.AssertEquals(t, "", "a2")
+	rows, err := res.RowsAffected()
+	helpers.AssertNoError(t, err)
+	helpers.AssertEqualsObject(t, int64(0), rows)
+
+	res, err = n.Exec("INSERT INTO test VALUES ('LOVE');")
+	helpers.AssertNoError(t, err)
+
+	rows, err = res.RowsAffected()
+	helpers.AssertNoError(t, err)
+	helpers.AssertEqualsObject(t, int64(1), rows)
 }
